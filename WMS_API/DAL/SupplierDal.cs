1e40983cfdbd969fb22d4950601cacea1480d960
o <o@LAPTOP-PRF64I3N>
2020-05-25 09:44:01 +0800

    完成控制器

--- WMS_API/DAL/SupplierDal.cs
+++ WMS_API/DAL/SupplierDal.cs
@@ -11,29 +11,36 @@ namespace dal
    public class SupplierDal
     {
         //显示供应商
-        public List<Supplier> CarrierShow()
+        public List<Supplier> SupplierShow()
         {
             string str = "select * from Supplier s join UserInfo u on s.CreateBy=u.CreateBy where c.IsDel=0";
             
             return DBHelper.GetToList<Supplier>(str);
         }
+        //反填供应商
+        public Supplier Find(int id)
+        {
+            string str = $"select * from Supplier s join UserInfo u on s.CreateBy=u.CreateBy where c.SupplierId={id}";
+
+            return DBHelper.GetToList<Supplier>(str)[0];
+        }
 
         //修改供应商(删除操作)
-        public int DelCarrier(int id)
+        public int DelSupplier(int id)
         {
             string str = $"update Supplier set IsDel=1 where SupplierId={id}";
             return DBHelper.ExecuteNonQuery(str);
         }
 
         //修改供应商信息
-        public int UptCarrier(Supplier s)
+        public int UptSupplier(Supplier s)
         {
             string str = $"update Supplier set SupplierName='{s.SupplierName}',[Address]='{s.Address}',Tel='{s.Tel}',SupplierPerson='{s.SupplierPerson}',Email='{s.Email}',Remark='{s.Remark}',ModifiedBy={s.ModifiedBy},ModifiedDate=GETDATE() where SupplierId={s.SupplierId}";
             return DBHelper.ExecuteNonQuery(str);
         }
 
         //新增供应商
-        public int AddCarrier(Supplier s)
+        public int AddSupplier(Supplier s)
         {
             string str = $"insert into Supplier values('{s.SupplierName}','{s.Address}','{s.Tel}','{s.SupplierPerson}',{s.SupplierLevel},'{s.Email}',{s.IsDel},'{s.Remark}',{s.CreateBy},getdate(),null,null)";
             return DBHelper.ExecuteNonQuery(str);
