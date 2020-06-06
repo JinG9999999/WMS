684cfb33c633b6474569742959d6bf0a8a2202c9
DELL <DELL@DESKTOP-BVD8H49>
2020-06-06 11:10:55 +0800

    1234567

--- WMS_API/DAL/ShenheDAL.cs
+++ WMS_API/DAL/ShenheDAL.cs
@@ -24,13 +24,13 @@ namespace DAL
         //审核出库
         public int Upte(Values m)
         {
-            string sql = string.Format("update Stockout set StockOutStatus=1, ModifiedBy='{0}', ModifiedDate='{1}' where StockInId={2}", m.name, m.times, m.id);
+            string sql = string.Format("update Stockout set StockOutStatus=1, ModifiedBy='{0}', ModifiedDate='{1}' where StockOutId={2}", m.name, m.times, m.id);
             return DBHelper.ExecuteNonQuery(sql);
         }
         //审核出库明细
         public int Uptee(Values m)
         {
-            string sql = string.Format("update Stockoutdetail set Status=1, AuditinId='{0}', AuditinTime='{1}', ModifiedBy='{2}', ModifiedDate='{3}' where StockInId={4}", m.name, m.times, m.name, m.times, m.id);
+            string sql = string.Format("update Stockoutdetail set Status=1, AuditinId='{0}', AuditinTime='{1}', ModifiedBy='{2}', ModifiedDate='{3}' where StockOutId={4}", m.name, m.times, m.name, m.times, m.id);
             return DBHelper.ExecuteNonQuery(sql);
         }
     }
--- WMS_API/Model/Stockout.cs
+++ WMS_API/Model/Stockout.cs
@@ -6,11 +6,10 @@ namespace Model
     public partial class Stockout
     {
         public int StockOutId { get; set; }
-        public string OrderNo { get; set; }
+        public int OrderNo { get; set; }
         public string StockOutType { get; set; }
-        public int? CustomerId { get; set; }
+        public int CustomerId { get; set; }
         public int? StockOutStatus { get; set; }
-        public bool? IsDel { get; set; }
         public string Remark { get; set; }
         public string CreateBy { get; set; }
         public DateTime? CreateDate { get; set; }
